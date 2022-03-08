70c7b1e39ad30ea326fc0dcbe4d4be7a0cfc0960
Max <blackruntr@gmail.com>
2022-03-08 19:18:40 +0100

    "init"

---
+++ src/palindrome/Scheisse.java
@@ -0,0 +1,13 @@
+package palindrome;
+
+public class Scheisse {
+	private void psvm() {
+		// TODO Auto-generated method stub
+		System.out.println("Hell oworld");
+	}
+	public static void main(String arg[]) {
+		System.out.println("Hell oworld");
+	}
+	
+
+}
