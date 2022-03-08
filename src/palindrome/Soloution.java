1f4074090fcdf5108f3a27790f49f81e623b230c
Max <blackruntr@gmail.com>
2022-03-08 21:35:51 +0100

    @@

--- src/palindrome/Soloution.java
+++ src/palindrome/Soloution.java
@@ -13,21 +13,14 @@ palindrome 123 true if 321 works
       System.out.println(l);
       String flippedword = "" ;
       char[] temp = new char[20]; //i think you can also put l in here but not sure lets work with 0 for now
-      for(int i = 0 ; i < l ; i++){
+      for(int i = l ; i < 0 ; i--){
     	  temp[i] = xword.charAt(i);
       }
       System.out.println("its put in the list");
-      for(int h = l ; h < 0 ; h--){
+      for(int h = l ; h > 0 ; h--){
     	  System.out.println(temp[h]);
       }
-      
-      /*
-       * ending session 
-       * i just need to check to the char[] s together
-       * 
-       */
-      
-      
+
        
         return false;
             
